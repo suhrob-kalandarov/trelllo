9a707776e98b3df1620e378a279c02872d116455
Abduvaliy <inosukebest1@example.com>
2025-04-28 15:14:31 +0500

    second commit

--- src/main/java/org/exp/trello/controllers/auth/VerificationController.java
+++ src/main/java/org/exp/trello/controllers/auth/VerificationController.java
@@ -1,8 +1,7 @@
-package org.exp.trello.controllers.home;
+package org.exp.trello.controllers.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.exp.trello.services.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
