f89d668c9063a65057612be6b899f7c6ed2d267c
Abduvaliy <inosukebest1@example.com>
2025-04-28 13:46:47 +0500

    second commit

---
+++ src/main/java/org/exp/trello/controllers/home/VerificationController.java
@@ -0,0 +1,21 @@
+package org.exp.trello.controllers.home;
+
+import lombok.RequiredArgsConstructor;
+import org.exp.trello.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class VerificationController {
+    private final EmailService emailService;
+
+    @GetMapping("/send-code")
+    public String sendCode(@RequestParam String email) {
+        String code = String.valueOf((int)(Math.random() * 9000) + 1000);
+        emailService.sendVerificationCode(email, code);
+        return "Verification code sent to " + email;
+    }
+}
