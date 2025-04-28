cce4ffc9c42d1a0f7915f583d5b446d4dc3b234c
Abduvaliy <inosukebest1@example.com>
2025-04-28 18:02:16 +0500

    verify and verificationPage added and added to db

--- src/main/java/org/exp/trello/controllers/auth/VerificationController.java
+++ src/main/java/org/exp/trello/controllers/auth/VerificationController.java
@@ -1,8 +1,14 @@
 package org.exp.trello.controllers.auth;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.exp.trello.models.entities.User;
+import org.exp.trello.repositories.UserRepository;
 import org.exp.trello.services.EmailService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class VerificationController {
     private final EmailService emailService;
+    private final UserRepository userRepository;
 
+    @Transactional
     @GetMapping("/send-code")
-    public String sendCode(@RequestParam String email) {
+    public String sendCode(@RequestParam String email, HttpSession session) {
         String code = String.valueOf((int)(Math.random() * 9000) + 1000);
         emailService.sendVerificationCode(email, code);
-        return "Verification code sent to " + email;
+        session.setAttribute("verificationCode", code);
+        return "auth/verification";
     }
+
+    @Transactional
+    @PostMapping("/verify")
+    public String verify(HttpSession session, @RequestParam String verificationCode, Model model) {
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/auth/login";
+        }
+        User registeredUser = (User) user;
+
+        String code = (String) session.getAttribute("verificationCode");
+        if (!code.equals(verificationCode)) {
+            model.addAttribute("error", "Verification code is incorrect. Please try again");
+            return "auth/verification";
+        }
+        registeredUser.setVerified(true);
+        userRepository.save(registeredUser);
+        return "auth/login";
+    }
+
 }
