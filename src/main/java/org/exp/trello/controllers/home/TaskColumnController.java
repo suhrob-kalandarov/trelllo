10243fe7469a32ef8f9a41277979d5107c6f7415
Suhrob <suhrob_kalandarov@outlook.com>
2025-04-30 21:18:32 +0500

    column edit/update, edit-column.html added and changed index.html, TaskColumnController.java, TaskRepository.java

--- src/main/java/org/exp/trello/controllers/home/TaskColumnController.java
+++ src/main/java/org/exp/trello/controllers/home/TaskColumnController.java
@@ -2,10 +2,14 @@ package org.exp.trello.controllers.home;
 
 import org.exp.trello.models.entities.TaskColumn;
 import org.exp.trello.repositories.TaskColumnRepository;
+import org.exp.trello.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/column")
@@ -13,6 +17,9 @@ public class TaskColumnController {
 
     @Autowired
     private TaskColumnRepository taskColumnRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
 
     @PostMapping("/create")
     public String createColumn(@RequestParam String name,
@@ -39,6 +46,41 @@ public class TaskColumnController {
     public String deleteColumn(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
         taskColumnRepository.deleteById(id);
         redirectAttributes.addFlashAttribute("successMessage", "Column deleted successfully");
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Integer id, Model model) {
+        Optional<TaskColumn> optionalColumn = taskColumnRepository.findById(id);
+
+        if (optionalColumn.isPresent()) {
+            TaskColumn column = optionalColumn.get();
+            Long taskCount = taskRepository.countByColumnId(column.getId());
+            model.addAttribute("column", column);
+            model.addAttribute("columnTaskCount", taskCount);
+            return "column/edit-column";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateColumn(@PathVariable Integer id,
+                               @RequestParam String name,
+                               @RequestParam(required = false, defaultValue = "false") boolean inActive,
+                               RedirectAttributes redirectAttributes) {
+        Optional<TaskColumn> columnOpt = taskColumnRepository.findById(id);
+
+        if (columnOpt.isPresent()) {
+            TaskColumn column = columnOpt.get();
+            column.setName(name);
+            column.setInActive(inActive);
+
+            taskColumnRepository.save(column);
+
+            redirectAttributes.addFlashAttribute("successMessage", "Column updated successfully");
+        }
+
         return "redirect:/";
     }
 }
--- src/main/java/org/exp/trello/repositories/TaskRepository.java
+++ src/main/java/org/exp/trello/repositories/TaskRepository.java
@@ -2,6 +2,8 @@ package org.exp.trello.repositories;
 
 import org.exp.trello.models.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
     List<Task> findAllByInActiveTrue();
+
+    @Query("SELECT COUNT(t) FROM Task t WHERE t.column.id = :columnId")
+    Long countByColumnId(@Param("columnId") Integer columnId);
 }
