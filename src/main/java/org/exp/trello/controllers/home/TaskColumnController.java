9d9e0905656e6f5aae15c36ade63a90bafb4ba4c
Suhrob <suhrob_kalandarov@outlook.com>
2025-04-30 19:15:46 +0500

    column create added index.html, TaskColumnController.java, TaskColumnRepository.java

---
+++ src/main/java/org/exp/trello/controllers/home/TaskColumnController.java
@@ -0,0 +1,37 @@
+package org.exp.trello.controllers.home;
+
+import org.exp.trello.models.entities.TaskColumn;
+import org.exp.trello.repositories.TaskColumnRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/column")
+public class TaskColumnController {
+
+    @Autowired
+    private TaskColumnRepository taskColumnRepository;
+
+    @PostMapping("/create")
+    public String createColumn(@RequestParam String name,
+                               @RequestParam(name = "active", defaultValue = "false") boolean active,
+                               RedirectAttributes redirectAttributes
+    ) {
+        // Get the highest position value
+        Integer maxPosition = taskColumnRepository.findMaxPosition();
+        int newPosition = (maxPosition != null) ? maxPosition + 1 : 1;
+
+        // Create new column
+        TaskColumn column = new TaskColumn();
+        column.setName(name);
+        column.setInActive(!active); // <-- Diqqat: inversiya
+        column.setPosition(newPosition);
+
+        taskColumnRepository.save(column);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Column created successfully");
+        return "redirect:/";
+    }
+}
--- src/main/java/org/exp/trello/repositories/TaskColumnRepository.java
+++ src/main/java/org/exp/trello/repositories/TaskColumnRepository.java
@@ -2,8 +2,13 @@ package org.exp.trello.repositories;
 
 import org.exp.trello.models.entities.TaskColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface TaskColumnRepository extends JpaRepository<TaskColumn, Integer> {
     List<TaskColumn> findAllByInActiveTrueOrderByPositionAsc();
+
+    @Query("SELECT MAX(c.position) FROM TaskColumn c")
+    Integer findMaxPosition();
 }
