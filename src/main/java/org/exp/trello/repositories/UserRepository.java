9f617358f9918614d16e81517dfed68006407c8f
Suhrob <suhrob_kalandarov@outlook.com>
2025-05-03 15:29:47 +0500

    UserController.java added and team updated

--- src/main/java/org/exp/trello/repositories/UserRepository.java
+++ src/main/java/org/exp/trello/repositories/UserRepository.java
@@ -4,11 +4,12 @@ import org.exp.trello.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    List<User> findAllByActiveTrue();
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
